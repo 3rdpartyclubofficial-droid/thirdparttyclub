247107518ba353b824b435baf83f992ecbadcc19
86gram <86gramsoft@gmail.com>
2025-12-14 12:19:20 +0900

    애널리스틱_이벤트_함수_등록

--- js/analytics.js
+++ js/analytics.js
@@ -4,7 +4,18 @@
 
   gtag('config', 'G-XTLE47DTXF');
 
-  function trackApplyClick() {
+
+//index 파일  이벤트 함수
+function trackEvent(eventName, eventLabel) {
+  gtag('event', eventName, {
+    event_category: 'index_link',
+    event_label: eventLabel
+  });
+}
+
+//wine 파일 버튼 이벤트 함수
+
+function trackApplyClick() {
   gtag('event', 'apply_click', {
     event_category: 'engagement',
     event_label: 'wine_meeting'
