8a4a2bf8c1af89d5f78717f683ad986a5d29b7be
86gram <86gramsoft@gmail.com>
2025-12-14 10:11:43 +0900

    애널리스틱_추가

---
+++ js/analytics.js
@@ -0,0 +1,12 @@
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XTLE47DTXF');
+
+  function trackApplyClick() {
+  gtag('event', 'apply_click', {
+    event_category: 'engagement',
+    event_label: 'wine_meeting'
+  });
+}
